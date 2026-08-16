e7846f3a81fc32f5183b634a5203042ef0af49c7
itsrituraz <itsriturazz@gmail.com>
2026-08-16 22:36:35 +0530

    Fix CORS origins

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,28 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const authRoutes = require('./routes/auth.routes');
-const musicRoutes = require('./routes/music.routes');
-const playlistRoutes = require("./routes/playlist.routes");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth.routes");
+const musicRoutes = require("./routes/music.routes");
+const playlistRoutes = require("./routes/playlist.routes");
 
 const app = express();
 
 app.use(
-    cors({
-      origin:[
-       "http://localhost:5173",
-       "https://audora-chi.versal.app",
-      ], 
-      credentials: true,
-    })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://audora-chi.vercel.app",
+    ],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.use('/api/auth', authRoutes);
-app.use('/api/music', musicRoutes);
-app.use('/api/playlists',playlistRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/music", musicRoutes);
+app.use("/api/playlists", playlistRoutes);
 
 module.exports = app;
